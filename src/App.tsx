30b11fbede448aa3ee1dd73912de3c081ecd5625
Jishnu <jishnu.s@deepnetsoft.com>
2023-06-06 21:36:05 +0530

    Configured routes

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="about" element={<About />}></Route>
+    </Routes>
+  );
+}
+
+export default App;
