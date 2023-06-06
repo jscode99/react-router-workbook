11f349a171ee3ef2593424f09fc18d2c6e444aaa
Jishnu Satheesh <jscode99@gmail.com>
2023-06-06 22:11:40 +0530

    Added order summary route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
+import OrderSummary from "./components/OrderSummary";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="about" element={<About />}></Route>
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+        <Route path="order-summary" element={<OrderSummary />}></Route>
+      </Routes>
+    </>
   );
 }
 
