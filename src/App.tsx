816afc58f349a80cffb6af115880e94ecdbda87e
Jishnu Satheesh <jscode99@gmail.com>
2023-06-07 08:11:21 +0530

    No Match Routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import OrderSummary from "./components/OrderSummary";
+import NoMatch from "./components/NoMatch";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="about" element={<About />}></Route>
-        <Route path="order-summary" element={<OrderSummary />}></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="order-summary" element={<OrderSummary />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </>
   );
---
+++ src/components/NoMatch.tsx
@@ -0,0 +1,4 @@
+
+export default function NoMatch() {
+  return <div>Page not found</div>;
+}
